ae5a72195d9ae65f8ec66498b171bd1c696eee58
Vincent <v.nassaux@nootos.com>
2018-07-24 05:40:53 +0200

    Revision : INSERT INTO  &  UPDATE SET WHERE

--- OpenClassRoomGROUP_3/Chap25.php
+++ OpenClassRoomGROUP_3/Chap25.php
@@ -1,6 +1,4 @@
 <?php
-
-
 ///////////////////////////////////////////////////////////
 /*  Programmation pour éviter de ùontrer au visiteurs, dont
 certains malveillants  */ /////////////////////////////////
@@ -14,14 +12,52 @@ catch (Exception $e)
         die('Erreur : ' . $e->getMessage());
 }
 
-
-
 ///////////////////////////////////////////////////////////
-/* Lecture de la table dans phpmyadmin grâce au langage SQL
-*/ ////////////////////////////////////////////////////////
-
+/* Récupération dans la table dans phpmyadmin */
+///////////////////////////////////////////////////////////
 $reponse = $bdd->query('SELECT * FROM jeux_video');
 
+///////////////////////////////////////////////////////////
+/* Requête INSERT INTO pour loger de nouvelles infos dans le tableau
+ ////////////////////////////////////////////////////////
+INSERT INTO jeux_video(ID, nom, possesseur, console, prix, nbre_joueurs_max,
+commentaires)
+VALUES('', 'Civilisation', 'Vincent', 'Mac', 45, 8, 'Conquêtes');
+Ci-dessus, Vous remarquerez que pour le premier champ (ID), j'ai laissé
+des apostrophes vides. C'est voulu : le champ a la propriété auto_increment,
+MySQL mettra donc le numéro d'ID lui-même. On pourrait même se passer du
+champ ID dans la requête :
 
+INSERT INTO jeux_video(nom, possesseur, console, prix, nbre_joueurs_max,
+commentaires) VALUES('Battlefield 1942', 'Patrick', 'PC', 45, 50, '2nde
+guerre mondiale')
 
+C'est encore plus simple ! Le champ ID sera de toute façon automatiquement
+rempli par MySQL, il est donc inutile de le lister.*/
+?>
+
+/////////////// INSERT INTO  ///////////////////////////////////////////////////
+<?php
+$req = $bdd->prepare("INSERT INTO jeux_video(nom, possesseur, console, prix,
+  nbre_joueurs_max, commentaires)VALUES(:nom, :possesseur, :console, :prix,
+    :nbre_joueurs_max, :commentaires)");
+$req->execute(array(
+  'nom' => $nom,
+  'possesseur' => $possesseur,
+  'console' => $console,
+  'nbre_joueurs_max' => $nbre_joueurs_max,
+  'commentaires' => $commentaires
+  ));
+echo "Le jeu a bien été ajouté !";
+?>
+
+////////////////// UPDATE SET WHERE  ///////////////////////////////////////////
+<?php
+$req = $bdd->prepare('UPDATE jeux_video SET prix = :nvprix,
+  nbre_joueurs_max = :nv_nb_joueurs WHERE nom = :nom_jeu');
+$req->execute(array(
+	'nvprix' => $nvprix,
+	'nv_nb_joueurs' => $nv_nb_joueurs,
+	'nom_jeu' => $nom_jeu
+	));
 ?>
